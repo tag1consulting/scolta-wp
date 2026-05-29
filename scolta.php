9d056c064f09e14002f0e6f3cda768efc359decf
Jeremy Andrews <jeremy@tag1consulting.com>
2026-05-29 15:09:36 -0800

    Open 1.0.2-dev

--- scolta.php
+++ scolta.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Scolta AI Search
  * Plugin URI:        https://www.tag1.com/scolta
  * Description:       Zero-infrastructure AI search with Pagefind, query expansion, summarization.
- * Version:       1.0.1
+ * Version:       1.0.2-dev
  * Requires at least: 6.1
  * Requires PHP:      8.1
  * Author:            Tag1 Consulting
@@ -15,7 +15,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'SCOLTA_VERSION', '1.0.1' );
+define( 'SCOLTA_VERSION', '1.0.2-dev' );
 define( 'SCOLTA_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'SCOLTA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SCOLTA_PLUGIN_FILE', __FILE__ );
